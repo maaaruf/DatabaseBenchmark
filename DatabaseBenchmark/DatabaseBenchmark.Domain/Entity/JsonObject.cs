a8e9c909ad69be55f431188c332bbc5a0ebd3f66
maaaruf <marufbdonline@gmail.com>
2020-12-27 12:05:05 +0600

    Added Id property

--- DatabaseBenchmark/DatabaseBenchmark.Domain/Entity/JsonObject.cs
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Entity/JsonObject.cs
@@ -7,6 +7,7 @@ namespace DatabaseBenchmark.Domain.Entity
 {
     public class JsonObject
     {
+        public int Id { get; set; }
         public string Key { get; set; }
         public string Value { get; set; }
     }
