70b8fe99ea5c51bbc63307225db324244ee98c0f
maaaruf <marufbdonline@gmail.com>
2020-12-28 14:15:17 +0600

    Added Mapping for productskey

---
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Mappings/ProductKeyMap.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseBenchmark.Domain.Entity;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseBenchmark.Domain.Mappings
+{
+    public class ProductKeyMap : ClassMap<ProductKey>
+    {
+        public ProductKeyMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.ProductsKey);
+            Table("ProductKeys");
+        }
+    }
+}
