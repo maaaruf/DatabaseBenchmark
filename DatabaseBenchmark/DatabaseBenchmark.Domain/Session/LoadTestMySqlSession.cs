9bd4fc20966f339fc4cb66ba9daca5d401e543a8
maaaruf <marufbdonline@gmail.com>
2020-12-28 14:13:56 +0600

    Added another session

---
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Session/LoadTestMySqlSession.cs
@@ -0,0 +1,41 @@
+﻿using DatabaseBenchmark.Domain.Mappings;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace DatabaseBenchmark.Domain.Session
+{
+    class LoadTestMySqlSession
+    {
+        private ISessionFactory _session;
+
+
+        public ISessionFactory CreateSession()
+        {
+
+            if (_session != null)
+            {
+                return _session;
+            }
+
+            string connection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+            FluentConfiguration _config = Fluently.Configure()
+               .Database(MySQLConfiguration.Standard.ConnectionString(connection))
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<JsonObjectMap>());
+
+
+            _session = _config.BuildSessionFactory();
+            return _session;
+        }
+
+        public ISession SessionOpen()
+        {
+            return CreateSession().OpenSession();
+        }
+    }
+}
