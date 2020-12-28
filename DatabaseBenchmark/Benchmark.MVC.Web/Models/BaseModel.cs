4ad839034fd27d7de61ce19f43508e75cf72bbad
maaaruf <marufbdonline@gmail.com>
2020-12-28 14:13:10 +0600

    Modified resopitory instance with generic session

--- DatabaseBenchmark/Benchmark.MVC.Web/Models/BaseModel.cs
+++ DatabaseBenchmark/Benchmark.MVC.Web/Models/BaseModel.cs
@@ -1,6 +1,7 @@
 ﻿using DatabaseBenchmark.Domain.Entity;
 using DatabaseBenchmark.Domain.Repositories;
 using DatabaseBenchmark.Domain.Service;
+using DatabaseBenchmark.Domain.Session;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,12 @@ namespace Benchmark.MVC.Web.Models
     {
         public RandomDataGeneratorService _randomDataGenerator { get; set; }
         public ObjectToJsonConverterService _objectToJsonConverter { get; set; }
-        public Repository<JsonObject> _jsonObjectRepository { get; set; }
+        public Repository<JsonObject, DBBenchmarkMySqlSession> _jsonObjectRepository { get; set; }
         public BaseModel()
         {
             _randomDataGenerator = new RandomDataGeneratorService();
             _objectToJsonConverter = new ObjectToJsonConverterService();
-            _jsonObjectRepository = new Repository<JsonObject>();
+            _jsonObjectRepository = new Repository<JsonObject, DBBenchmarkMySqlSession>(new DBBenchmarkMySqlSession());
         }
     }
 
