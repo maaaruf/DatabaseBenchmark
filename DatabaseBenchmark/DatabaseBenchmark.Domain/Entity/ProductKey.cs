4927da1bc5150d0edf4e6f8b07fb3468d58cba49
maaaruf <marufbdonline@gmail.com>
2020-12-28 14:14:53 +0600

    Added entity

---
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Entity/ProductKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseBenchmark.Domain.Entity
+{
+    public class ProductKey
+    {
+        public int Id { get; set; }
+        public string ProductsKey { get; set; }
+    }
+}
