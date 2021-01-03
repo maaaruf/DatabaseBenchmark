4240a3c633f91d6ce5e03c8a70e1990202577faf
maaaruf <marufbdonline@gmail.com>
2021-01-03 17:09:14 +0600

    Added Mapping for ProductObject

---
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Mappings/ProductsObjectMap.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseBenchmark.Domain.Entity;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseBenchmark.Domain.Mappings
+{
+    public class ProductsObjectMap : ClassMap<ProductsObject>
+    {
+        public ProductsObjectMap()
+        {
+            Id(x => x.ProductKey);
+            Map(x => x.ProductValue);
+            Table("products");
+        }
+    }
+}
