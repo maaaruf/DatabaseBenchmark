ad4de0c8867685858b0a60f36cbf7809c9d022ac
maaaruf <marufbdonline@gmail.com>
2021-01-03 17:06:16 +0600

    Implimented with Interface

---
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Service/IJsonToObjectConverterService.cs
@@ -0,0 +1,12 @@
+﻿using DatabaseBenchmark.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseBenchmark.Domain.Service
+{
+    public interface IJsonToObjectConverterService<T>
+    {
+        IList<T> Convert(string data);
+    }
+}
