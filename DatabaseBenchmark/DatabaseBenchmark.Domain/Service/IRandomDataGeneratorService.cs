19d70f1aa2e8aa118f0b4e17f5d427150d2eda8b
maaaruf <marufbdonline@gmail.com>
2021-01-03 17:05:12 +0600

    Implimented with Interface

---
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Service/IRandomDataGeneratorService.cs
@@ -0,0 +1,12 @@
+﻿using DatabaseBenchmark.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseBenchmark.Domain.Service
+{
+    public interface IRandomDataGeneratorService
+    {
+        IList<Product> GenerateProducts(int productCount);
+    }
+}
