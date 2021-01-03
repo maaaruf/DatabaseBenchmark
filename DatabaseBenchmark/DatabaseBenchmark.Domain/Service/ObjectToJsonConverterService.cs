fa80a67e912070bef32499ac8eb9b4840338476a
maaaruf <marufbdonline@gmail.com>
2021-01-03 17:18:14 +0600

    Added comment for convert method

--- DatabaseBenchmark/DatabaseBenchmark.Domain/Service/ObjectToJsonConverterService.cs
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Service/ObjectToJsonConverterService.cs
@@ -8,6 +8,11 @@ namespace DatabaseBenchmark.Domain.Service
 {
     public class ObjectToJsonConverterService : IObjectToJsonConverterService
     {
+        /// <summary>
+        /// Converts any object to json
+        /// </summary>
+        /// <param name="rawObject"></param>
+        /// <returns>json</returns>
         public string Convert(object rawObject)
         {
             string json = JsonSerializer.Serialize(rawObject);
