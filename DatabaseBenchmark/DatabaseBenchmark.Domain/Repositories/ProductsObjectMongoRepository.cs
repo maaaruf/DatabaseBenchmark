55b9a7050bd870d418b45616fc1106c710b3fd0f
maaaruf <marufbdonline@gmail.com>
2021-01-03 09:46:51 +0600

    Implimented with MongoRepository

--- DatabaseBenchmark/DatabaseBenchmark.Domain/Repositories/ProductsObjectMongoRepository.cs
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Repositories/ProductsObjectMongoRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using DatabaseBenchmark.Domain.Entity;
+using DatabaseBenchmark.Domain.Session;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DatabaseBenchmark.Domain.Repositories
 {
-    public class ProductsObjectMongoRepository 
+    public class ProductsObjectMongoRepository : RepositoryMongo<ProductsObject, DBBenchmarkMongoSession>
     {
+        public ProductsObjectMongoRepository(DBBenchmarkMongoSession session, string table= "products") : base(new DBBenchmarkMongoSession("dbb"), table)
+        {
+        }
     }
 }
