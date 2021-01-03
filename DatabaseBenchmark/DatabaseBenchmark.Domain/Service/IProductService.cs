a49eff146a20ab17c0cb0db1776ed167e5768328
maaaruf <marufbdonline@gmail.com>
2021-01-03 17:07:05 +0600

    Implimented with Interface

--- DatabaseBenchmark/DatabaseBenchmark.Domain/Service/IProductService.cs
+++ DatabaseBenchmark/DatabaseBenchmark.Domain/Service/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseBenchmark.Domain.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,10 @@ namespace DatabaseBenchmark.Domain.Service
 {
     public interface IProductService
     {
+        IList<ProductsObject> GenerateJsonProducts(int dataCount);
+        TimeSpan InsertProducts(IList<ProductsObject> products);
+        ProductsObject GetSingleKeysProduct(string key);
+        void InsertKey(string key);
+
     }
 }
